308fc9069a2030a01ed7fff6c4b6a9b05de824ca
simonp20 <parker_simon@brown.edu>
2022-04-26 14:31:04 -0400

    automatically generated commit message

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,9 +1,10 @@
 import gzip
 import h5py
+import librosa
 
 def get_data():
     f = h5py.File('data/MillionSongSubset/A/A/A/TRAAAAW128F429D538.h5', 'r')
     print(list(f.keys()))
     print(list(f['analysis'].keys()))
-    print(list(f['analysis']['segments_pitches'].keys()))
+    print(list(f['analysis']['segments_pitches']))
     

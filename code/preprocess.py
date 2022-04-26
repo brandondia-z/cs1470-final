3d53ddfa23d150a1b6f8aa740a38f4f52512a8a4
simonp20 <parker_simon@brown.edu>
2022-04-26 14:20:25 -0400

    automatically generated commit message

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,3 +4,6 @@ import h5py
 def get_data():
     f = h5py.File('data/MillionSongSubset/A/A/A/TRAAAAW128F429D538.h5', 'r')
     print(list(f.keys()))
+    print(list(f['analysis'].keys()))
+    print(list(f['metadata'].keys()))
+    print(list(f['musicbrainz'].keys()))

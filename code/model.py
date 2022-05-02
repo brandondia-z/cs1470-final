eff115572d8bf13e566fa6bcd90f9e1316bad81d
Brandon Diaz <77746846+brandondia-z@users.noreply.github.com>
2022-05-02 12:53:19 -0400

    model started

--- code/model.py
+++ code/model.py
@@ -1,6 +1,22 @@
 import torch as torch
 import numpy as np
 
+# Creates Model class
 class Model(torch.nn.Module):
-    def __init__(self):
+    def __init__(self): #Might alter parameters
+        super(Model, self).__init__()
+
+    def call():
+        model = nn.Sequential(torch.OrderedDict([
+          ('conv1', torch.nn.Conv2d()),#Numbers
+          ('BN1', torch.nn.BatchNorm2d()),
+          ('activate1', torch.nn.ELU(alpha=1.0)),
+          ('conv2', torch.nn.Conv2d()),#Numbers
+          ('activate2', torch.nn.ELU(alpha=1.0))
+          ('conv3', torch.nn.Conv2d()),#Numbers
+          ('activate3', torch.nn.ELU(alpha=1.0))
+
+        ]))
+    
+    def loss():
 

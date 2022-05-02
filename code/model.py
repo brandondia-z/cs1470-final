9e2346ec79da699fa51116b81ab1970ea7fa37a7
Colin Savage <colin_f_savage@brown.edu>
2022-05-02 14:36:56 -0400

    yuh

--- code/model.py
+++ code/model.py
@@ -1,12 +1,15 @@
-import torch as torch
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Creates Model class
-class Model(torch.nn.Module):
+class Model(nn.Module):
     def __init__(self): #Might alter parameters
         super(Model, self).__init__()
-
-    def call():
         model = nn.Sequential(torch.OrderedDict([
           ('conv1', torch.nn.Conv2d()),#Numbers
           ('BN1', torch.nn.BatchNorm2d()),
@@ -15,8 +18,8 @@ class Model(torch.nn.Module):
           ('activate2', torch.nn.ELU(alpha=1.0))
           ('conv3', torch.nn.Conv2d()),#Numbers
           ('activate3', torch.nn.ELU(alpha=1.0))
-
         ]))
-    
-    def loss():
+        #Insert hyperparameters here
 
+    def call(self, x):
+        return model(x)

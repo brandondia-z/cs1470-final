9a5dde5e8e7ecc9998df9ac9b04c45e4953fc5b5
Brandon Diaz <77746846+brandondia-z@users.noreply.github.com>
2022-05-02 13:41:07 -0400

    conv blocks done

--- code/model.py
+++ code/model.py
@@ -6,17 +6,63 @@ class Model(torch.nn.Module):
     def __init__(self): #Might alter parameters
         super(Model, self).__init__()
 
-    def call():
-        model = nn.Sequential(torch.OrderedDict([
-          ('conv1', torch.nn.Conv2d()),#Numbers
-          ('BN1', torch.nn.BatchNorm2d()),
-          ('activate1', torch.nn.ELU(alpha=1.0)),
-          ('conv2', torch.nn.Conv2d()),#Numbers
-          ('activate2', torch.nn.ELU(alpha=1.0))
-          ('conv3', torch.nn.Conv2d()),#Numbers
-          ('activate3', torch.nn.ELU(alpha=1.0))
+        self.zeroPad = torch.nn.ZeroPad2d(padding=(37,37,0,0)) # (PadLeft, PadRight, PadTop, PadBottom)
+        self.batchNorm1 = torch.nn.BatchNorm2d(num_features=1366) # TODO: Fix Params
 
-        ]))
+        #Conv Block 1
+        self.conv1 = torch.nn.functional.conv2d(input=1366, weight=64, stride=3, padding='same') #TODO: input param
+        self.batchNorm1 = torch.nn.BatchNorm2d(num_features=1) # TODO: Fix Params
+        self.elu1 = torch.nn.ELU(alpha=1.0)
+        self.maxPool1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.dropout1 = torch.nn.Dropout(p=0.1)
+
+        #Conv Block 2
+        self.conv2 = torch.nn.Conv2d(in_channels=1366, out_channels=128, kernel_size=3) #TODO: input param
+        self.batchNorm2 = torch.nn.BatchNorm2d(num_features=1) # TODO: Fix Params
+        self.elu2 = torch.nn.ELU(alpha=1.0)
+        self.maxPool2 = torch.nn.MaxPool2d(kernel_size=(3,3), stride=(3,3))
+        self.dropout2 = torch.nn.Dropout(p=0.1)
+
+        #Conv Block 3
+        self.conv3 = torch.nn.Conv2d(in_channels=1366, out_channels=128, kernel_size=3) #TODO: input param
+        self.batchNorm3 = torch.nn.BatchNorm2d(num_features=1) # TODO: Fix Params
+        self.elu3 = torch.nn.ELU(alpha=1.0)
+        self.maxPool3 = torch.nn.MaxPool2d(kernel_size=(4,4), stride=(4,4))
+        self.dropout3 = torch.nn.Dropout(p=0.1)
+
+        #Conv Block 4
+        self.conv3 = torch.nn.Conv2d(in_channels=1366, out_channels=128, kernel_size=3) #TODO: input param
+        self.batchNorm3 = torch.nn.BatchNorm2d(num_features=1) # TODO: Fix Params
+        self.elu3 = torch.nn.ELU(alpha=1.0)
+        self.maxPool3 = torch.nn.MaxPool2d(kernel_size=(4,4), stride=(4,4))
+        self.dropout3 = torch.nn.Dropout(p=0.1)
+
+        # model = nn.Sequential(torch.OrderedDict([
+        #   ('conv1', torch.nn.Conv2d()),#Numbers
+        #   ('BN1', torch.nn.BatchNorm2d(axis=1, mode=2)),
+        #   ('activate1', torch.nn.ELU(alpha=1.0)),
+        #   ('MaxPool1', torch.nn.MaxPool2d((2, 4))),
+
+        #   ('conv2', torch.nn.Conv2d()),#Numbers
+        #   ('BN2', torch.nn.BatchNorm2d(axis=1, mode=2)),
+        #   ('activate2', torch.nn.ELU(alpha=1.0)),
+        #   ('MaxPool2', torch.nn.MaxPool2d((3, 4))),
+
+        #   ('conv3', torch.nn.Conv2d()),#Numbers
+        #   ('BN3', torch.nn.BatchNorm2d(axis=1, mode=2)),
+        #   ('activate3', torch.nn.ELU(alpha=1.0))
+        #   ('MaxPool3', torch.nn.MaxPool2d((2, 5))),
+
+        #   ('Flatten', torch.nn.Flatten()),
+
+        #   ('Dense', torch.nn.Linear()), #Params (in_feats, out_feats)
+        #   ('DenseActivation', torch.nn.Sigmoid())
+
+        # ]))
+
+
+    def call():
+        
     
     def loss():
 
